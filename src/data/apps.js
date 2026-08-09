a4410ee52e6e6836df494b3731b5eb0806db5790
TaimoorAli3921 <alit8029@gmail.com>
2026-08-09 19:52:23 +0500

    CP-100 Analyzer update text

--- src/data/apps.js
+++ src/data/apps.js
@@ -20,7 +20,7 @@ const apps = [
     {
     id: "oncp100",
     name: "Online CP-100 Analyzer",
-    description: "Analyze Online CP-100 and get totals of online banks.",
+    description: "Analyze Online or Direct CP-100 and get totals of banks for single date or for multiple dates .",
     category: "Banking",
     icon: "💻",
     path: "apps/oncp100/index.html",
